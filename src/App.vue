1f185098a4cc1199e14015c0e356b271f6175b0e
Rui Posse <ruiposse@gmail.com>
2020-07-19 20:45:29 +0100

    feat: added vue, vue-router and vite

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header />
+  <router-view />
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>home</h1>
+  <router-link to="/about">About</router-link>
+</template>
+
+<script>
+export default {}
+</script>
